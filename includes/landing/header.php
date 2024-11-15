5543490efa22ea629910f860ac74c805c69533f8
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2024-11-15 16:32:20 +0100

    Add landing page navbar for improved navigation and user access

---
+++ includes/landing/header.php
@@ -0,0 +1,21 @@
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">Events</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/organize.php">Organize</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/create.php">Create</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/pricing.php">Pricing</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/blog.php">Blog</a></li>
+                    <li class="nav-item"><a class="nav-link btn btn-primary text-white ms-2"
+                            href="/auth/register.php">Sign Up</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
--- includes/shared/header.php
+++ includes/shared/header.php
@@ -3,7 +3,6 @@
     <link href="../../css/styles.css" rel="stylesheet" />
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
 </head>
-</head>
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
 
     <!-- Sidebar Toggle-->
